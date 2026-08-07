36e0587d5915d55419dce8e0c5fea8f3620f2349
Niols <niols@niols.fr>
2026-08-07 18:07:10 +0200

    add missing deps `libcurl4-openssl-dev` and `libsnmp-dev`

--- src/overlays/external/debian.nix
+++ src/overlays/external/debian.nix
@@ -139,6 +139,7 @@ pkgs
   "libcairo2-dev" = cairo.dev;
   "libcapnp-dev" = capnproto;
   "libcurl4-gnutls-dev" = curlWithGnuTls.dev;
+  "libcurl4-openssl-dev" = curlFull.dev;
   "libdw-dev" = elfutils.dev;
   "libev-dev" = libev;
   "libevent-dev" = libevent.dev;
@@ -277,6 +278,7 @@ pkgs
   "libshine-dev" = shine;
   "libshp-dev" = shapelib;
   "libsnappy-dev" = snappy.dev;
+  "libsnmp-dev" = net-snmp.dev;
   "libsodium-dev" = libsodium.dev;
   "libsoundtouch-dev" = soundtouch;
   "libsource-highlight-dev" = sourceHighlight.dev;
